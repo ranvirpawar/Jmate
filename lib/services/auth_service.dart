e19bd5509b790ad2b1575b56583a25105819f7bf
Ranvir Pawar <123987654+ranvirpawar@users.noreply.github.com>
2024-06-05 10:48:52 +0530

    backend: auth_service

---
+++ lib/services/auth_service.dart
@@ -0,0 +1,13 @@
+class AuthService{
+    
+    //firebase auth and firestore instance 
+    final FirebaseAuth _auth = FirebaseAuth.instance;
+    final FirebaseFirestore  _firestore = FirebaseFirestore.instance   
+
+    // sign in method
+
+    // sign up method
+
+    // sign out method
+
+}
